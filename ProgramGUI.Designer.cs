63d4085aebbc3ca02729ab8b90487a8842717427
tentoesdown <39019486+tentoesdown@users.noreply.github.com>
2018-05-08 21:33:55 -0500

    Add files via upload

---
+++ ProgramGUI.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ChanScraper
+{
+    partial class ProgramGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgramGUI));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnScrape = new System.Windows.Forms.Button();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(142, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(646, 26);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thread URL:";
+            // 
+            // btnScrape
+            // 
+            this.btnScrape.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScrape.Location = new System.Drawing.Point(286, 430);
+            this.btnScrape.Name = "btnScrape";
+            this.btnScrape.Size = new System.Drawing.Size(207, 93);
+            this.btnScrape.TabIndex = 2;
+            this.btnScrape.Text = "SCRAPE";
+            this.btnScrape.UseVisualStyleBackColor = true;
+            this.btnScrape.Click += new System.EventHandler(this.btnScrape_Click);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgress.Location = new System.Drawing.Point(356, 236);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(55, 32);
+            this.lblProgress.TabIndex = 3;
+            this.lblProgress.Text = "0/0";
+            // 
+            // folderBrowser
+            // 
+            this.folderBrowser.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // ProgramGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.btnScrape);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(822, 600);
+            this.MinimumSize = new System.Drawing.Size(822, 600);
+            this.Name = "ProgramGUI";
+            this.Text = "Chan Scraper";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnScrape;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+    }
+}
+
---
+++ ProgramGUI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using AngleSharp.Extensions;
+using System.Windows.Forms;
+
+namespace ChanScraper
+{
+    public partial class ProgramGUI : Form
+    {
+        HtmlParser parser = new HtmlParser();
+        
+        public ProgramGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnScrape_Click(object sender, EventArgs e)
+        {
+            folderBrowser.ShowDialog();
+            lblProgress.Text = folderBrowser.SelectedPath;
+            Scrape(txtURL.Text);
+        }
+
+        private void Scrape(string url)
+        {
+            string page = DownloadString(url);
+            var document = parser.Parse(page);
+            var links = document.GetElementsByClassName("fileText");
+
+            int current = 0, max = links.Length;
+
+            foreach (var t in links)
+            {
+                lblProgress.Text = (++current).ToString() + "/" + max.ToString();
+                lblProgress.Update();
+                string imageLink = "http:" + t.FirstElementChild.GetAttribute("href");
+                string imageOut = folderBrowser.SelectedPath+ "\\" + t.FirstElementChild.Text();
+                Console.WriteLine("LINK: " + imageLink);
+                Console.WriteLine("FILE: " + imageOut);
+                DownloadFile(imageLink, imageOut);
+            }
+            lblProgress.Text = "Done!";
+            lblProgress.Update();
+        }
+
+        static void DownloadFile(string url, string fileName)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("User-Agent", "Mozilla/5.0");
+                client.DownloadFile(url, fileName);
+            }
+        }
+
+        static string DownloadString(string url)
+        {
+            string str;
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("User-Agent", "Mozilla/5.0");
+                str = client.DownloadString(url);
+            }
+
+            return str;
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
